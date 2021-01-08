59386d1a59bf79aff364e32eff01d28d36806623
marwonn <marc@wonneberger.xyz>
2021-01-08 11:41:35 +0100

    update gitignore

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    """
+    user credentials configuration 
+    """
+    USERNAME = "your username"
+    SECRET_KEY = "1234456"
+    SPOTIFY_CLIENT_ID = "your client id"
+    SPOTIFY_CLIENT_SECRET = "your client secret"
